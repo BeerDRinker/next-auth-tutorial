8eac755d53384ee80e19ce530fafaf74c1ebb767
Rostyslav Palii <rostyslav.palii@l7informatics.com>
2024-04-04 10:22:20 +0300

    add routes, middleware auth and redirect logic

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,40 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "./routes";
 
 export const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = Boolean(req.auth);
-  console.log("ROUTE: ", req.nextUrl.pathname);
-  console.log("isLoggedIn: ", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return NextResponse.next();
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return NextResponse.next();
 });
 
 // Optionally, don't invoke Middleware on some paths
-// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
